6ab0518b43892c4381fdc480d934b27c6b250f58
George0099s <avakyan.georgiy00@gmail.com>
2020-01-14 17:29:47 +0300

    checked email

--- app/src/main/java/com/avla/app/Interface/Server.java
+++ app/src/main/java/com/avla/app/Interface/Server.java
@@ -9,13 +9,18 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.HTTP;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface Server {
+    @FormUrlEncoded
     @POST("public_api/auth/check_email_registered")
-    Call<EmailValidate> addEmail(@Query("token") String token, @Body String email);
+    Call<EmailValidate> addEmail(@Query("token") String token, @Field("email") String email);
 }
